16e9af53d71b4b7924f0a055ae50cd3f66434720
Robert <r.walerczak@gmail.com>
2022-01-19 19:27:25 +1100

    Add create character content, remove store

--- controllers/jaunt_controller.py
+++ controllers/jaunt_controller.py
@@ -7,3 +7,8 @@ jaunt_controller = Blueprint(
 @jaunt_controller.route('/home')
 def home():
     return render_template('home.html')
+
+
+@jaunt_controller.route('/learn_the_basics')
+def learn_the_basics():
+    return render_template('/learn_the_basics.html')
--- controllers/session_controller.py
+++ controllers/session_controller.py
@@ -12,9 +12,6 @@ def loginpage():
     return render_template('login.html', error=error)
 
 
-# TRYING TO FIGURE OUT HOW TO STAY LOGGED IN ONCE YOU REGISTER A NEW ACCOUNT
-
-# LOOK HERE
 @session_controller.route('/sessions/create', methods=["POST"])
 def login():
     email = request.form.get('email')
@@ -34,3 +31,44 @@ def login():
 def logout():
     session.clear()
     return redirect('/')
+
+
+@session_controller.route('/create_page', methods=["GET", "POST"])
+def create_page():
+    return render_template('create_page.html')
+
+
+@session_controller.route('/create_character', methods=["POST"])
+def create_character():
+    user_id = request.form.get('user_id')
+    name = request.form.get('name')
+    rift_counter = request.form.get('rift_counter')
+    good_luck = request.form.get('good_luck')
+    bad_luck = request.form.get('bad_luck')
+    stuff = request.form.get('stuff')
+    lessons = request.form.get('lessons')
+    facet_one = request.form.get('facet_one')
+    facet_two = request.form.get('facet_two')
+    body = request.form.get('body')
+    body_resistance = request.form.get('body_resistance')
+    body_coordination = request.form.get('body_coordination')
+    body_vigour = request.form.get('body_vigour')
+    intellect = request.form.get('intellect')
+    intellect_resistance = request.form.get('intellect_resistance')
+    intellect_ingenuity = request.form.get('intellect_ingenuity')
+    presence = request.form.get('presence')
+    presence_resistance = request.form.get('presence_resistance')
+    presence_allure = request.form.get('presence_allure')
+    presence_guile = request.form.get('presence_guile')
+    advantage_one = request.form.get('advantage_one')
+    advantage_two = request.form.get('advantage_two')
+    utility_one = request.form.get('utility_one')
+    utility_two = request.form.get('utility_two')
+
+    error = request.args.get('error', "Character created successfully!")
+    return render_template('create_page.html', error=error)
+
+
+@session_controller.route('/profile_settings', methods=["GET", "POST"])
+def profile_settings():
+    return render_template('profile_settings.html')
