2cd715f63b1d0287c29e9fddd907241a09ce40a9
Robert <r.walerczak@gmail.com>
2022-01-21 19:23:58 +1100

    Add edit character functionality

--- controllers/jaunt_controller.py
+++ controllers/jaunt_controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, redirect
-from models.character import get_all_characters, delete_character_from_database
+from flask import Blueprint, render_template, session, redirect, request
+from models.character import get_all_characters, get_character, delete_character_from_database, complete_character_edit_database
 
 
 jaunt_controller = Blueprint(
@@ -25,9 +25,39 @@ def delete_character(id):
 
 
 @jaunt_controller.route('/<id>/edit_character', methods=["POST"])
-def edit_character():
-    # PUT IN ALL THE VARIABLES HERE TO BE PARSED IN THE EDIT_CHARACTER PARAMETERS
-    # edit_character(id)
+def edit_character(id):
+    character_dictionary = get_character(id)
+    return render_template('edit_character.html', character_dictionary=character_dictionary)
+
+
+@jaunt_controller.route('/complete_character_edit/<id>', methods=["POST"])
+def complete_character_edit(id):
+    name = request.form.get('name')
+    stuff = request.form.get('stuff')
+    lessons = request.form.get('lessons')
+    good_luck = request.form.get('good_luck')
+    bad_luck = request.form.get('bad_luck')
+    rift_counter = request.form.get('rift_counter')
+    body = request.form.get('body')
+    body_resistance = request.form.get('body_resistance')
+    body_coordination = request.form.get('body_coordination')
+    body_vigour = request.form.get('body_vigour')
+    intellect = request.form.get('intellect')
+    intellect_resistance = request.form.get('intellect_resistance')
+    intellect_ingenuity = request.form.get('intellect_ingenuity')
+    intellect_scrutiny = request.form.get('intellect_scrutiny')
+    presence = request.form.get('presence')
+    presence_resistance = request.form.get('presence_resistance')
+    presence_allure = request.form.get('presence_allure')
+    presence_guile = request.form.get('presence_guile')
+    facet_one = request.form.get('facet_one')
+    facet_two = request.form.get('facet_two')
+    advantage_one = request.form.get('advantage_one')
+    advantage_two = request.form.get('advantage_two')
+    utility_one = request.form.get('utility_one')
+    utility_two = request.form.get('utility_two')
+    complete_character_edit_database(id, name, stuff, lessons, good_luck, bad_luck, rift_counter, body, body_resistance, body_coordination, body_vigour, intellect, intellect_resistance,
+                                     intellect_ingenuity, intellect_scrutiny, presence, presence_resistance, presence_allure, presence_guile, facet_one, facet_two, advantage_one, advantage_two, utility_one, utility_two)
     return redirect('/')
 
 
@@ -39,13 +69,20 @@ def edit_character():
 #     price = request.form.get("price")
 #     description = request.form.get("description")
 #     image_url = request.form.get("image_url")
-
-#     # UPDATE
 #     update_food(id, name, price, description, image_url)
-
 #     return redirect('/')
 
-
+# @food_controller.route('/foods/<id>', methods=["POST"])
+# def update(id):
+#     if not session.get('user_id'):
+#         return redirect('/login')
+#     name = request.form.get("name")
+#     price = request.form.get("price")
+#     description = request.form.get("description")
+#     image_url = request.form.get("image_url")
+#     # UPDATE
+#     update_food(id, name, price, description, image_url)
+#     return redirect('/')
 # @food_controller.route('/foods/<id>/delete', methods=["POST"])
 # def delete(id):
 #     if not session.get('user_id'):
--- models/character.py
+++ models/character.py
@@ -12,16 +12,16 @@ def get_all_characters(user_id):
     return results
 
 
-def edit_character(id, name, price, description, image_url):
-    database.sql_write("UPDATE foods set name = %s, price = %s, description = %s, image_url = %s WHERE id = %s", [
-        name,
-        float(price) * 100,
-        description,
-        image_url,
-        id
-    ])
+def get_character(id):
+    results = database.sql_select(
+        "SELECT * FROM characters WHERE id = %s", [id])
+    return results
 
-# DELETE FOOD FROM DB
+
+def complete_character_edit_database(id, name, stuff, lessons, good_luck, bad_luck, rift_counter, body, body_resistance, body_coordination, body_vigour, intellect, intellect_resistance, intellect_ingenuity, intellect_scrutiny, presence, presence_resistance, presence_allure, presence_guile, facet_one, facet_two, advantage_one, advantage_two, utility_one, utility_two):
+    database.sql_write("UPDATE characters set name = %s, stuff = %s, lessons = %s, good_luck = %s, bad_luck = %s, rift_counter = %s, body = %s, body_resistance = %s, body_coordination = %s, body_vigour = %s, intellect = %s, intellect_resistance = %s, intellect_ingenuity = %s, intellect_scrutiny = %s, presence = %s, presence_resistance = %s, presence_allure = %s, presence_guile = %s, facet_one = %s, facet_two = %s, advantage_one = %s, advantage_two = %s, utility_one = %s, utility_two = %s WHERE id = %s", [
+        name, stuff, lessons, good_luck, bad_luck, rift_counter, body, body_resistance, body_coordination, body_vigour, intellect, intellect_resistance, intellect_ingenuity, intellect_scrutiny, presence, presence_resistance, presence_allure, presence_guile, facet_one, facet_two, advantage_one, advantage_two, utility_one, utility_two, id
+    ])
 
 
 def delete_character_from_database(id):
